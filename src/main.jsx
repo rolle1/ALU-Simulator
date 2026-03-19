ec6111223729fe486cd5d6db250c12056dea388e
rolle1 <34388098+rolle1@users.noreply.github.com>
2026-03-19 15:57:10 -0400

    Rename import from ALUSimulator to alu-simulator

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import ALUSimulator from './ALUSimulator'
+import ALUSimulator from './alu-simulator'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
